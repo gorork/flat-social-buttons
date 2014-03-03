2f8bac12f48119d58d97788f96a524569f4b4c72
Nadi <qodovo@gmail.com>
2014-03-02 22:38:56 -0800

    Initial commit to the github repo

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+        
+        $( "#email" ).click(function() {
+             $( "form" ).toggle( "fade", 800 );
+        });
+        
+        $( "#email" ).click(function() {
+             $('#email').toggleClass('yellow-back');
+        });
+    }
+);
